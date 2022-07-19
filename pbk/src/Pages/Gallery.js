2ec5fa2110d4f8cbcaa2c283116738af40049320
“your <it20154226@my.sliit.lk>
2022-07-19 12:41:05 +0530

    Gallery Done

--- pbk/src/Pages/Gallery.js
+++ pbk/src/Pages/Gallery.js
@@ -6,98 +6,134 @@ export default function Gallery() {
     <div className="container aos-init aos-animate" data-aos="fade-up">
     <div className='content-wrap'>
         <div className='row'>
+            <div className='col-md-4 p-3'>
+                <div className='thumbnail'>
+                <a href="https://bit.ly/3oeYaNq" target="_blank">
+                <img src="https://bit.ly/3oeYaNq" alt="Responsive image" className="img-thumbnail"/>
+                </a>
+                </div>
+            </div>
+            <div className='col-md-4 p-3'>
+                <div className='thumbnail'>
+                <a href="https://bit.ly/3yNjWwE" target="_blank">
+                <img src="https://bit.ly/3yNjWwE" alt="Responsive image" className="img-thumbnail"/>
+                </a>
+                </div>
+            </div>
+            <div className='col-md-4 p-3'>
+                <div className='thumbnail'>
+                <a href="https://bit.ly/3PxUYYG" target="_blank">
+                <img src="https://bit.ly/3PxUYYG" alt="Responsive image" className="img-thumbnail"/>
+                </a>
+                </div>
+            </div>
             <div className='col-md-4 p-3' >
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3IVgCnF" alt="Responsive image" className="img-thumbnail" target="_blank">
+                <img src="https://bit.ly/3IVgCnF" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3OwyG93" target="_blank">
+                <img src="https://bit.ly/3OwyG93" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3RUOtkS" target="_blank">
+                <img src="https://bit.ly/3RUOtkS" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
         
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3OiAfXG" target="_blank">
+                <img src="https://bit.ly/3OiAfXG" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3uXppj0" target="_blank">
+                <img src="https://bit.ly/3uXppj0" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3zghP5N" target="_blank">
+                <img src="https://bit.ly/3zghP5N" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3OjYNjf" target="_blank">
+                <img src="https://bit.ly/3OjYNjf" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3OlDRZ4" target="_blank">
+                <img src="https://bit.ly/3OlDRZ4" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3RG9eR4" target="_blank">
+                <img src="https://bit.ly/3RG9eR4" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3PkSCg5" target="_blank">
+                <img src="https://bit.ly/3PkSCg5" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3aUBAX7" target="_blank">
+                <img src="https://bit.ly/3aUBAX7" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://bit.ly/3oe35hn" target="_blank">
+                <img src="https://bit.ly/3oe35hn" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
             <div className='col-md-4 p-3'>
                 <div className='thumbnail'>
-                <a href="" target="_blank">
-                <img src="https://www.w3schools.com/w3images/lights.jpg" alt="Responsive image" className="img-thumbnail"/>
+                <a href="https://res.cloudinary.com/ddt5pwieo/image/upload/v1658214406/Portraits%20By%20Kavindu/Arts/PicsArt_08-06-12.09.49_vbvnjb.jpg" target="_blank">
+                <img src="https://res.cloudinary.com/ddt5pwieo/image/upload/v1658214406/Portraits%20By%20Kavindu/Arts/PicsArt_08-06-12.09.49_vbvnjb.jpg" alt="Responsive image" className="img-thumbnail"/>
                 </a>
                 </div>
             </div>
+            <div className='col-md-4 p-3'>
+                <div className='thumbnail'>
+                <a href="https://res.cloudinary.com/ddt5pwieo/image/upload/v1658214399/Portraits%20By%20Kavindu/Arts/1652077572038_c4uuym.png" target="_blank">
+                <img src="https://res.cloudinary.com/ddt5pwieo/image/upload/v1658214399/Portraits%20By%20Kavindu/Arts/1652077572038_c4uuym.png" alt="Responsive image" className="img-thumbnail"/>
+                </a>
+                </div>
+            </div>
+            <div className='col-md-4 p-3'>
+                <div className='thumbnail'>
+                <a href="https://res.cloudinary.com/ddt5pwieo/image/upload/v1658214404/Portraits%20By%20Kavindu/Arts/IMG20200723093544_yqtpqz.jpg" target="_blank">
+                <img src="https://res.cloudinary.com/ddt5pwieo/image/upload/v1658214404/Portraits%20By%20Kavindu/Arts/IMG20200723093544_yqtpqz.jpg" alt="Responsive image" className="img-thumbnail"/>
+                </a>
+                </div>
+            </div>
+        
         
         
         </div>
