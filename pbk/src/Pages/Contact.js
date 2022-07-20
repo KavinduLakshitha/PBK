4d9af788f1be06cd5138979e0cc1d7d04e04362c
“your <it20154226@my.sliit.lk>
2022-07-20 12:27:32 +0530

    Contact Page Done!

--- pbk/src/Pages/Contact.js
+++ pbk/src/Pages/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import "../styles/contact.css"
 
@@ -7,10 +7,17 @@ import "jquery/dist/jquery.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "../styles/navbar.css";
 
-// npm i @emailjs/browser
+const Result = () => {
+  return(
+    <p className="result">Your message has been submitted successfully!</p>
+  )
+}
 
 const Contact = () => {
+  
   const form = useRef();
+
+  const [result, showResult] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -31,38 +38,49 @@ const Contact = () => {
           console.log(error.text);
         }
       );
-  };
+      e.target.reset();
+      showResult(true);
+    };
 
-  return (
-    
-      <div className="d-flex justify-content-center">
-        <form ref={form} onSubmit={sendEmail}>
-          
-          <div className="form-group">
-            <label className="label" >Name</label>
-            <input className="input" type="text" name="full_name" />
-          </div>
+  // hide result
 
-          <div className="form-group">
-            <label className="label">Email</label>
-            <input className="input" type="email" name="email" />
-          </div>         
+  setTimeout(() => {
+    showResult(false)  
+  }, 5000)
 
-          <div className="form-group">
-            <label className="label">Phone Number</label>
-            <input className="input" type="number" name="phone" />
-          </div>
-                    
-          <div className="form-group">
-            <label className="label">Message</label>
-            <textarea className="textarea" name="message" />
-          </div>
-          
-          <button type="submit" class="btn btn-secondary" value="Send">Submit</button>
-          
-        </form>
-      </div>
-    
+  return ( 
+        <div className="form-container">
+          <form ref={form} onSubmit={sendEmail}>
+            <div class="form-group">
+            <label for="full_name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="full_name" name="full_name" required/>
+            </div>
+            
+            <div class="form-group">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required/>
+            </div>
+
+            <div class="form-group">
+            <label for="phone" class="form-label">Phone</label>
+            <input type="number" class="form-control" id="phone" name="phone" required/>
+            </div>            
+            
+            <div class="form-group">
+            <label for="message" class="form-label">Message</label>
+            <textarea class="form-control" id="message" name="message" required/>
+            </div>
+                  
+            <div class="form-group">
+            <button class="btn btn-secondary" id="btn" type="submit" value="Send">Submit</button>
+
+            <div className="row">{
+              result ? <Result/> : null
+            }</div>
+
+            </div>
+          </form>
+        </div>
   );
 };
 
