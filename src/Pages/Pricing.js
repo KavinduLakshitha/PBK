5b9a9ac8532e18bb182b4bc25b5ee37676f026d2
Kavindu Marasinghe <it20154226@my.sliit.lk>
2025-03-01 08:42:00 +0530

    Update Pricing.js

--- src/Pages/Pricing.js
+++ src/Pages/Pricing.js
@@ -19,63 +19,56 @@ export default function Pricing() {
         </thead>
         <tbody>
           {/* 8x12 Details */}
-
           <tr>
             <td rowSpan="2">8x12</td>
             <td>Normal</td>
-            <td>4000</td>
-            <td>6000</td>
+            <td>5500</td>
+            <td>7500</td>
           </tr>
-
           <tr>
             <td>Couple</td>
-            <td>6000</td>
-            <td>8000</td>
+            <td>7500</td>
+            <td>9500</td>
           </tr>
 
           {/* 10x15 Details */}
-
           <tr>
             <td rowSpan="2">10x15</td>
             <td>Normal</td>
-            <td>5000</td>
-            <td>7500</td>
-          </tr>
-
-          <tr>
-            <td>Couple</td>
             <td>6500</td>
             <td>9000</td>
           </tr>
+          <tr>
+            <td>Couple</td>
+            <td>8500</td>
+            <td>11000</td>
+          </tr>
 
-          {/* 10x15 Details */}
-
+          {/* 12x18 Details */}
           <tr>
             <td rowSpan="3">12x18</td>
             <td>Normal</td>
-            <td>6000</td>
-            <td>9000</td>
+            <td>7500</td>
+            <td>11000</td>
           </tr>
-
           <tr>
             <td>Couple</td>
-            <td>9000</td>
-            <td>12000</td>
+            <td>11000</td>
+            <td>14000</td>
           </tr>
-
           <tr>
             <td>
               Family
               <br />
               (5 people max.)
             </td>
-            <td>4000 per each</td>
-            <td>Art Price + Rs.3000</td>
+            <td>5500 per each</td>
+            <td>Art Price + Rs.3500</td>
           </tr>
         </tbody>
       </table>
 
-      <p class="courier" style={{ marginTop: "30px" }}>
+      <p className="courier" style={{ marginTop: "30px" }}>
         *Islandwide delivery available. &nbsp;Courier charges may vary depending
         on the size and the location.
       </p>
